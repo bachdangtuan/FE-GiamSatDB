68d4aa844899b6e47f9c10d0b13646b0ffea7f10
Your Name <you@example.com>
2023-06-06 11:11:46 +0700

    update

--- src/app/pages/nhom-benh-vien/bv-pk40/bv-pk40.component.ts
+++ src/app/pages/nhom-benh-vien/bv-pk40/bv-pk40.component.ts
@@ -1,15 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Injector, OnInit} from '@angular/core';
+import {SyncQueryParam} from "../../../core/decorators/syncParams.decorator";
+import {FormGroup} from "@angular/forms";
+import {BreadcrumbItem} from "../../../shared/page-title/page-title/page-title.model";
+import {ActivatedRoute, Router} from "@angular/router";
+import {FormSearchLogHelper} from "../../../core/helpers/formSearchLog.helper";
+import {LogBackupService} from "../../../core/service/log-backup.service";
+import {COLUMN_LOG} from "../../../core/constants/common";
 
 @Component({
-  selector: 'app-bv-pk40',
-  templateUrl: './bv-pk40.component.html',
-  styleUrls: ['./bv-pk40.component.scss']
+    selector: 'app-bv-pk40',
+    templateUrl: './bv-pk40.component.html',
+    styleUrls: ['./bv-pk40.component.scss']
 })
 export class BvPk40Component implements OnInit {
 
-  constructor() { }
+    @SyncQueryParam({
+        parseIgnore: ["status"],
+    })
+    public formSearchAndFilter: FormGroup;
+    pageTitle: BreadcrumbItem[] = [];
+    limit: any
+    pageCurrent: any
+    records: any[] = [];
+    columns: any[] = [];
+    totalItems: any;
 
-  ngOnInit(): void {
-  }
+    constructor(
+        public injector: Injector,
+        private route: ActivatedRoute,
+        private formService: FormSearchLogHelper,
+        private LogService: LogBackupService,
+        public router: Router,
+    ) {
+        this.formSearchAndFilter = formService.form;
+        this.getLogList()
+        this.initTableCofig()
+    }
+
+    ngOnInit(): void {
+        this.pageTitle = [{label: 'Log Backup Jira', path: '/', active: true}];
+        this.formSearchAndFilter.patchValue({
+            hostName: 'patroni1',
+            nameDatabase: 'sakura_pk40',
+            page: 1,
+            limit: 6,
+        });
+    }
+
+    initTableCofig(): void {
+        this.columns = COLUMN_LOG
+    }
+
+    getLogList() {
+        const params$ = this.route.queryParams;
+        console.log('params$', params$)
+        params$.subscribe(param => {
+            this.LogService.getListLogging(param).subscribe(res => {
+                console.log(res)
+                this.totalItems = res?.totalItems
+                this.records = res.data
+                this.limit = res.limit
+                this.pageCurrent = res?.thisPage
+            })
+        })
+    }
+
+
+    updatePage(data: number): void {
+        this.formService.form.patchValue({
+            page: data,
+        });
+    }
+
 
 }
