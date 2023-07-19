e593191c1344fcc0481e4b8a8bbf3b4f2333c71d
Your Name <you@example.com>
2023-07-19 14:22:15 +0700

    cập nhật giao diện mới

--- src/app/pages/nhom-benh-vien/bv-vietnhat/bv-vietnhat-info/bv-vietnhat-info.component.ts
+++ src/app/pages/nhom-benh-vien/bv-vietnhat/bv-vietnhat-info/bv-vietnhat-info.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProjectService} from "../../../../core/service/project.service";
 
 @Component({
-  selector: 'app-bv-vietnhat-info',
-  templateUrl: './bv-vietnhat-info.component.html',
-  styleUrls: ['./bv-vietnhat-info.component.scss']
+    selector: 'app-bv-vietnhat-info',
+    templateUrl: './bv-vietnhat-info.component.html',
+    styleUrls: ['./bv-vietnhat-info.component.scss']
 })
 export class BvVietnhatInfoComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+    serverPhysical: any
+    user: any
+    projectID = 18;
+    isDataLoaded: boolean = false;
+
+    constructor(
+        private ProjectService: ProjectService
+    ) {
+        this.getDetailCompany()
+    }
+
+
+    ngOnInit(): void {
+    }
+
+    getDetailCompany() {
+        this.ProjectService.getCompanyDetail(this.projectID).pipe().subscribe(res => {
+            this.user = res[0]
+            this.serverPhysical = res[0]['listPhysicalMachine']
+            console.log('res', res[0]['listPhysicalMachine'])
+            this.isDataLoaded = true;
+        })
+    }
 
 }
